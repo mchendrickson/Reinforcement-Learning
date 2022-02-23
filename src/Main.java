c39b941e7f78f19cdf73e1106b7e44c7247b608e
joshunger1 <jhunger@wpi.edu>
2022-02-22 19:34:13 -0500

    Cleanup of Main

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,27 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println("Bazinga");
-	public static void main(String[] args) {
 
+		if(Objects.equals(args[0], "board.txt")) {
+			try {
+				File board = new File("C:\\Users\\Josh\\Documents\\CS4341Assignment4\\Boards\\board.txt");
+				BoardGenerator board1 = new BoardGenerator();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		int boardheight = 0;
 		int boardwidth = 0;
 
 		//reads the second input as the filename and opens the file
 		String Filename = args[0];
-		try {
-			File myObj = new File("C:\\Users\\Josh\\Documents\\CS4341Assignment4\\Boards\\" + Filename);
-			Scanner myReader = new Scanner(myObj);
-			int i = 0;
-			//while the file is open, we parse through it to calculate the board size
-			while (myReader.hasNextLine()) {
-				String data = myReader.nextLine();
-				String[] spaces = data.split("\t");
-				boardwidth = spaces.length;
-				for(String s : spaces) {
-					//System.out.println(s);
-				}
-				boardheight++;
-				i++;
-			}
-			myReader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-		//System.out.println(boardheight);
-		//System.out.println(boardwidth);
-
-		//We reopen the file to collect the values at each board space to place in a 2x2 array
-		Board board = new Board(boardheight, boardwidth);
-		try {
-			File myObj = new File("C:\\Users\\Josh\\Documents\\CS4341Assignment4\\Boards\\" + Filename);
-			Scanner myReader = new Scanner(myObj);
-			int i = 0;
-			while (myReader.hasNextLine()) {
-				String data = myReader.nextLine();
-				String[] spaces = data.split("\t");
-				for(int j = 0; j < spaces.length; j++) {
-					try {
-						board.board[i][j] = Integer.parseInt(spaces[j]);
-					}
-					catch (NumberFormatException e) {
-						e.printStackTrace();
-					}
-				}
-				i++;
-			}
-			myReader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-
-		String fileName = "C:\\Users\\Starkiller PC\\Desktop\\CS4341Assignment4\\sample.txt";
+		String fileName = "C:\\Users\\Josh\\Documents\\CS4341Assignment4\\Boards\\board.txt";
 		//float timeToRun = Float.parseFloat(args[1]);
 		//float probDesiredDirection = Float.parseFloat(args[2]);
 		//float constantReward = Float.parseFloat(args[3]);
@@ -70,6 +30,7 @@ public class Main {
 
 
 	}
+
 	private static void openFile(String fileName) throws FileNotFoundException {
 
 		File myObj = new File(fileName);
